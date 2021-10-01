c4f11fd1b83e359c2e0bf33db9f31baebcdf41a0
Trevor Howard <trevorhoward2@hotmail.com>
2021-10-01 13:22:16 -0600

    Create task modal+form implemented

--- src/components/Tasks/CreateTask/createTask.js
+++ src/components/Tasks/CreateTask/createTask.js
@@ -1,29 +1,18 @@
 import React from "react";
 import "./createTask.scss";
-import axios from 'axios';
+
+import CreateTaskModal from "../TaskModals/CreateTaskModal/createTaskModal";
+import useModal from "../TaskModals/utilities/useModal";
 
 const CreateTask = () => {
-
-    const handleCreateNewTask = async () => {
-        console.log('New task button clicked')
-        // data backend is expecting
-        // {
-        //     "title": "Fifth generated task",
-        //     "description": "The fifth task created with the api",
-        //     "priority": "high",
-        //     "column_id": 1
-        // }
-
-        // try {
-        //     const newTask = await axios.post(process.env.REACT_APP_API_URL)
-        // } catch (err) {
-        //     console.error(err)
-        // }
-    }
+    const { isShowing, toggle } = useModal()
 
     return (
-        <div className="create-btn" onClick={handleCreateNewTask}>
-            Create new task
+        <div>
+            <div className="create-btn" onClick={() => toggle()}>
+                Create new task
+            </div>
+            <CreateTaskModal isShowing={isShowing} hide={toggle} />
         </div>
     )
 }
--- src/components/Tasks/TaskModals/CreateTaskModal/createTaskModal.js
+++ src/components/Tasks/TaskModals/CreateTaskModal/createTaskModal.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./createTaskModal.scss";
+import axios from "axios";
 
-const CreateTaskModal = ({ isShowing, hide, data }) =>
-    isShowing
-        ? ReactDOM.createPortal(
+const CreateTaskModal = ({ isShowing, hide }) => {
+
+    const useInput = (initialValue) => {
+        const [value, setValue] = useState(initialValue);
+
+        const handleChange = (e) => {
+            setValue(e.target.value);
+        };
+
+        return [value, handleChange];
+    };
+
+    const [title, setTitle] = useInput('')
+    const [description, setDescription] = useInput('')
+    const [priority, setPriority] = useInput('low')
+
+    const createTask = async (e) => {
+        e.preventDefault();
+        const createTaskData = {
+            title: title,
+            description: description,
+            priority: priority,
+            column_id: 1
+        }
+        try {
+            await axios.post(process.env.REACT_APP_API_URL, createTaskData)
+            hide()
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    return (
+        isShowing ? ReactDOM.createPortal(
             <React.Fragment>
                 <div className="modal-overlay" />
                 <div className="modal-wrapper">
@@ -20,13 +52,46 @@ const CreateTaskModal = ({ isShowing, hide, data }) =>
                             </button>
                         </div>
                         <div className="modal-data">
-                            Create fields will go here
+                            <div className="modal-title">
+                                Create new Task
+                            </div>
+                            <form>
+                                <div className="new-task-data">
+                                    <div>Title:</div>
+                                    <input
+                                        placeholder="Name"
+                                        value={title}
+                                        onChange={setTitle}
+                                        required
+                                    />
+                                    <div>Description:</div>
+                                    <textarea
+                                        placeholder="Description"
+                                        value={description}
+                                        onChange={setDescription}
+                                        required
+                                    />
+                                    <div>Priority:</div>
+                                    <select
+                                        onChange={setPriority}
+                                    >
+                                        <option value="low">Low</option>
+                                        <option value="medium">Medium</option>
+                                        <option value="high">High</option>
+                                    </select>
+                                </div>
+                                <div type="submit" className="create-btn-modal" onClick={createTask}>
+                                    Create
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </React.Fragment>,
             document.body
         )
-        : null;
+            : null
+    )
+}
 
 export default CreateTaskModal;
